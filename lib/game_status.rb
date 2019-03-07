90549e3e97cb71f56c22b292518c8cf577cbe705
Blaine Lawson <blaine.lawson@gmail.com>
2019-03-07 17:07:48 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -41,12 +41,6 @@ def won?(board)
   # end
 
   WIN_COMBINATIONS.find do |win_combo|
-    # win_combo.find do |index|
-    #   binding.pry
-    #   position_taken?(board, index)
-    # end
-    # &&
-    # binding.pry
     position_taken?(board, win_combo[0]) && position_taken?(board, win_combo[1]) && position_taken?(board, win_combo[2]) &&
     board[win_combo[0]] == board[win_combo[1]] && board[win_combo[1]] == board[win_combo[2]]
   end
