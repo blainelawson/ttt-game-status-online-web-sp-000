509d4690a1f36c0e94848fe5b32d6e21d61f2ae8
Blaine Lawson <blaine.lawson@gmail.com>
2019-03-07 16:11:10 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -21,7 +21,7 @@ def won?(board)
   WIN_COMBINATIONS.find do |win_combo|
     win_combo.all? {|index| board[index] == "O"}
   end
-binding.pry
+# binding.pry
   if win  == nil || win == false
     win =
     WIN_COMBINATIONS.find do |win_combo|
