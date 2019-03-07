20be1ec463f785f90fe6ef8ece3888e5891dd4fa
Blaine Lawson <blaine.lawson@gmail.com>
2019-03-07 15:28:00 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -1,6 +1,26 @@
-# Helper Method
+require "pry"
 def position_taken?(board, index)
   !(board[index].nil? || board[index] == " ")
 end
 
+
 # Define your WIN_COMBINATIONS constant
+WIN_COMBINATIONS = [
+  [0,1,2], #top row
+  [3,4,5], #middle row
+  [6,7,8], #bottom row
+  [0,4,8], #down diagonal
+  [2,4,6], #up diagonal
+  [0,3,6], #first column
+  [1,4,7], #middle column
+  [2,5,8]  #right column
+]
+
+def won?(board)
+  WIN_COMBINATIONS.find do |win_combo|
+    win_combo.all? {|index| board[index] == "O" || board[index] == "X"}
+  end
+
+
+
+end
